a1d818f17e745720af93b582edbd5f9aa35bb928
github-Hamid <h.rahimi.1953@gmail.com>
2022-11-28 12:57:49 -0500

    second

--- server.js
+++ server.js
@@ -8,20 +8,20 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/Airbnb/api", router);
-
+app.use(express.static(path.join(__dirname, "client/build")));
 //app.use("*",(req, res)=>{res.status(404).json({error : "Not Found!"})});
 
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  // All the javascript and css files will be read and served from this folder
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   // Set static folder
+//   // All the javascript and css files will be read and served from this folder
+//   app.use(express.static("client/build"));
 
-  // index.html for all page routes  html or routing and naviagtion
-  app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "../../client", "build", "index.html")
-    );
-  });
-}
+//   // index.html for all page routes  html or routing and naviagtion
+//   app.get("*", (req, res) => {
+//     res.sendFile(
+//       path.resolve(__dirname, "../../client", "build", "index.html")
+//     );
+//   });
+// }
 
 export default app;
